f973e62df4abb9600f505ed04a8d3e955351cc1c
ddenis87 <dizdok@yandex.ru>
2020-06-03 20:57:13 +0900

    Add formFind

---
+++ src/components/formFind.vue
@@ -0,0 +1,18 @@
+<template>
+  <form>
+    <hr>
+      <div class="find">
+        <input type="text" placeholder="Укажите фамилию для поиска" v-model="fio"/>
+        <input type="button" value="Найти..." />
+      </div>
+    <hr>
+  </form>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    fio: ''
+  })
+}
+</script>
--- src/views/opeka.vue
+++ src/views/opeka.vue
@@ -1,43 +1,72 @@
 <template>
   <div>
     <h2>Отдел опеки и попечительства</h2>
+    <formFind />
     <form>
-      <hr>
-      <div class="find">
-        <input type="text" placeholder="Укажите фамилию для поиска" v-model="fio"/>
-        <input type="button" value="Найти..." @click="findRes"/>
-      </div>
-      <hr>
       <span>{{ lineRes }}</span>
+      <table 
+        v-for="row in arrRes"
+         v-bind:key="row.id">
+        <tr>
+          <td colspan="2">
+            <span>ФИО, дата рождения</span>
+            <input type="text" v-model="row.fio" disabled/>
+          </td>
+        </tr>
+      </table>
     </form>
   </div>
 </template>
 
 <script>
-
+import formFind from "@/components/formFind"
 export default {
   data: () => ({
     lineRes: "Нет записей удовлетворяющих поиск",
     arrRes: [
-      {fio: "Смирнова Елена Сергеевна, 1979",1:"",2:"Благовещенск, ул. Калинина 142 кв. 125",3:"05.08.2016, Благовещенский городской суд, 2-8105/2016",4:"",5:"Смирнов Александр Сергеевич, 13.02.2006",6:"",7:"Благовещенск;лишение родительских прав"}
+      {id: "1", fio: "Смирнова Елена Сергеевна, 1979",1:"",2:"Благовещенск, ул. Калинина 142 кв. 125",3:"05.08.2016, Благовещенский городской суд, 2-8105/2016",4:"",5:"Смирнов Александр Сергеевич, 13.02.2006",6:"",7:"Благовещенск", 8:"лишение родительских прав"},
+      {id: "2", fio: "Смирнова Галина Вячеславовна, 1979",1:"",2:"Благовещенск, ул. Калинина 142 кв. 125",3:"05.08.2016, Благовещенский городской суд, 2-8105/2016",4:"",5:"Смирнов Александр Сергеевич, 13.02.2006",6:"",7:"Благовещенск", 8:"лишение родительских прав"}
     ],
-    fio: ''
   }),
+  components: {
+    formFind
+  },
   methods: {
-    findRes() {
-      let request = new XMLHttpRequest();
-	    request.open('POST','php/opeka.php', true);
-	    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-      request.send(`fio=${this.fio}`);
-      request.onload = () => {
-        // let arrRes = request.responseText.split("#");
-        console.log(request.responseText);
-        console.log(request.responseText.split("#"));
-        // console.log(JSON.parse(request.responseText));
-      }
-	    
-      console.log(this.fio)
+    findRes(data) {
+      // let request = new XMLHttpRequest();
+	    // request.open('POST','php/opeka.php', true);
+	    // request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+      // request.send(`fio=${this.fio}`);
+      // request.onload = () => {
+      //   // let arrRes = request.responseText.split("#");
+      //   console.log(request.responseText);
+      //   console.log(request.responseText.split("#"));
+      //   // console.log(JSON.parse(request.responseText));
+      // }
+	    this.lineRes = "Поиск по: " + data + "...";
+      console.log(data);
     }
   }
 }
 </script>
+
+<style scoped>
+span, input {
+  display: block;
+}
+
+span {
+  font-size: 12px;
+  color: grey;
+}
+
+input {
+  width: 300px;
+  border: 0px solid grey;
+  border-bottom: 2px solid grey;
+  padding: 5px;
+  color: black;
+  background-color: white;
+}
+
+</style>
