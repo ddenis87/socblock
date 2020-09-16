ed615b4c4cee34d4da85b72ff3c98e171a351a1c
ddenis87 <dizdok@yandex.ru>
2020-09-16 20:27:55 +0900

    Add file ocenka-report__list

---
+++ src/components/units/ocenka/ocenka-report__list.vue
@@ -0,0 +1,54 @@
+<template>
+  <fieldset>
+    <!-- <legend>Отчет</legend> -->
+    <table>
+      <!-- столбцы - вынесенные решения -->
+      <tr>
+        <td class="empty-first"></td>
+        <td v-for="(row, index) in arrReshenie" 
+            :key="index" 
+            class="title-collumn"><div class="vertical-text">{{ row.CNAME }}</div>
+        </td>
+        <td class="title-collumn"><div class="vertical-text">Всего вынесено результатов работы</div></td>
+      </tr>
+      <!-- ---------------------------- -->
+
+      <!-- ----------------строки - тер.органы---------------- -->
+      <template v-for="(row, rowIndex) in arrReport">
+          <tr :key="rowIndex">
+            <td class="title-row" :class="{'title-row-mru': (+row[0]) ? true : false }" >{{ row[1] }}</td>
+            
+                <td v-for="col in arrReshenie.length" 
+                    :key="col + 1000"
+                    class="count-content"
+                    :class="{'title-row-mru': (+row[0]) ? true : false }">{{ (row[col + 1]) ? row[col + 1] : '0' }}</td> 
+            
+            <td class="count-content itog"
+              :class="{'title-row-mru': (+row[0]) ? true : false }">{{ sumCollumn(row) }}</td>
+          </tr>
+      </template>
+      <!-- ------------------------------------------------ -->
+      <tr>
+        <td class="table-footer">Всего по территориям</td>
+        <template v-for="(row, index) in arrReportAll">
+          <td  :key="index"
+                class="table-footer">{{ row }}</td>
+        </template>
+        <td class="table-footer">{{ sumItog(arrReportAll) }}</td>
+      </tr>
+      <tr>
+
+      </tr>
+    </table>
+  </fieldset>
+</template>
+
+<script>
+export default {
+  name: 'ocenkaReportList',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/ocenka/ocenka-report.vue
+++ src/views/ocenka/ocenka-report.vue
@@ -19,56 +19,11 @@
     <div class="report__control">
       <ocenka-report-control></ocenka-report-control>
     </div>
-    
-    <!-- <report-control @selectedMRU="selectedMRU"
-                    @selectedDistrict="selectedDistrict"
-                    @selectedReshenie="selectedReshenie"
-                    @selectedLetter="selectedLetter"
-                    @buildingReport="buildingReport"></report-control> -->
+    <div class="report__list">
+      <ocenka-report-list></ocenka-report-list>
+    </div>
 
 
-    <fieldset>
-      <!-- <legend>Отчет</legend> -->
-      <table>
-        <!-- столбцы - вынесенные решения -->
-        <tr>
-          <td class="empty-first"></td>
-          <td v-for="(row, index) in arrReshenie" 
-              :key="index" 
-              class="title-collumn"><div class="vertical-text">{{ row.CNAME }}</div>
-          </td>
-          <td class="title-collumn"><div class="vertical-text">Всего вынесено результатов работы</div></td>
-        </tr>
-        <!-- ---------------------------- -->
-
-        <!-- ----------------строки - тер.органы---------------- -->
-        <template v-for="(row, rowIndex) in arrReport">
-            <tr :key="rowIndex">
-              <td class="title-row" :class="{'title-row-mru': (+row[0]) ? true : false }" >{{ row[1] }}</td>
-              
-                 <td v-for="col in arrReshenie.length" 
-                     :key="col + 1000"
-                     class="count-content"
-                     :class="{'title-row-mru': (+row[0]) ? true : false }">{{ (row[col + 1]) ? row[col + 1] : '0' }}</td> 
-              
-              <td class="count-content itog"
-                :class="{'title-row-mru': (+row[0]) ? true : false }">{{ sumCollumn(row) }}</td>
-            </tr>
-        </template>
-        <!-- ------------------------------------------------ -->
-        <tr>
-          <td class="table-footer">Всего по территориям</td>
-          <template v-for="(row, index) in arrReportAll">
-            <td  :key="index"
-                 class="table-footer">{{ row }}</td>
-          </template>
-          <td class="table-footer">{{ sumItog(arrReportAll) }}</td>
-        </tr>
-        <tr>
-
-        </tr>
-      </table>
-    </fieldset>
     <div class="warning-insert" :class="{'is-visible' : (isWarning) ? true : false}">
       Не выбран ни один территориальный орган или МРУ
     </div>
@@ -80,10 +35,11 @@ import dateRange from '@/components/elements/date-range';
 import ocenkaReportType from '@/components/units/ocenka/ocenka-report__type';
 import ocenkaReportFilter from '@/components/units/ocenka/ocenka-report__filter';
 import ocenkaReportControl from '@/components/units/ocenka/ocenka-report__control';
+import ocenkaReportList from '@/components/units/ocenka/ocenka-report__list';
 export default {
   name: 'OcenkaReport',
   components: {
-    dateRange, ocenkaReportType, ocenkaReportFilter, ocenkaReportControl,
+    dateRange, ocenkaReportType, ocenkaReportFilter, ocenkaReportControl, ocenkaReportList
   },
   data: function() {
     return {
